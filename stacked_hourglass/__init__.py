5ec1277092ec9d07131902bcd887f6a86c638569
Michael Painter <michaelpainter.1994@gmail.com>
2018-07-26 22:19:46 -0700

    Add init file for stacked hourglass

---
+++ stacked_hourglass/__init__.py
@@ -0,0 +1,4 @@
+# Imports to expose from "import .twod_threed" rather than having to use "import .twod_threed.main" for example
+from example.mpii import main as mpii_main
+from example.lsp import main as lsp_main
+from example.mscoco import main as mscoco_main
